f04099379852c672a5dd57d04b0e2b8df5c4842e
Dmitriy <lochnessathome@gmail.com>
2025-07-25 22:51:30 +0300

    http-сервер, доступ извне

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,13 @@ FROM dockerhub.timeweb.cloud/golang:1.24.5
 
 WORKDIR /usr/src/app
 
-# COPY go.mod go.sum ./
-COPY go.mod ./
+COPY go.mod go.sum ./
 RUN go mod download
 
 COPY . .
-RUN go build -v -o /usr/local/bin/app ./...
+RUN CGO_ENABLED=0 GOOS=linux go build -v -o /usr/local/bin/app ./...
+
+EXPOSE 80
 
 CMD ["app"]
 
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
-package main
+package main 
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+const httpPort = 80
 
 func main() {
-	fmt.Println("HELLO")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
 }
